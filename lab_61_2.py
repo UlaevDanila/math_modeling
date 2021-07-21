04c8c037dfc54111420d39b325f8d4b61a08c314
UlaevDanila <80589144+UlaevDanila@users.noreply.github.com>
2021-07-21 15:28:15 +0000

    add

--- lab_61_2.py
+++ lab_61_2.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def elipse():
-    f = np.arange(0, np.pi*8, np.pi/100, float)
+    f = np.linspace(0, np.pi*8, 100, float)
     p = float(input("Введите значение p "))
     e = float(input("Введите значение e "))
     r = p/(1+e*np.cos(f))
